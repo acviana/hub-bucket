5efa410062b20d2f6fe91a3089b3ea43f1674f46
Alex C. Viana <alexcostaviana@gmail.com>
2019-04-01 14:18:27 -0500

    Refactored GitHub API call and got first page of paginated results.

--- app.py
+++ app.py
@@ -2,35 +2,58 @@ import os
 
 import requests
 
-
-def main(github_username):
-    query='''
-    query UnpaginatedData($username: String!){
-      user(login: $username) {
-        totalRepositories: repositories {
-          totalCount
+paginated_query='''
+query NonPaginatedResources($username: String!) {
+  user(login: $username) {
+    repositories(first: 100) {
+      edges {
+        node {
+          name
+          stargazers{
+            totalCount
+          }
         }
-        forkedRespositories: repositories(isFork: true) {
-          totalCount
-        }
-        originalRepositories: repositories(isFork: false) {
-          totalCount
-        }
-        followers {
-          totalCount
-        }
-        following {
-          totalCount
-        }
-        starsGiven: starredRepositories {
-          totalCount
-        }
-        issues(states: OPEN) {
-          totalCount
-        }
+        cursor
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
       }
     }
-    '''
+  }
+}
+'''
+
+unpaginated_query='''
+query UnpaginatedData($username: String!){
+  user(login: $username) {
+    totalRepositories: repositories {
+      totalCount
+    }
+    forkedRespositories: repositories(isFork: true) {
+      totalCount
+    }
+    originalRepositories: repositories(isFork: false) {
+      totalCount
+    }
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    starsGiven: starredRepositories {
+      totalCount
+    }
+    issues(states: OPEN) {
+      totalCount
+    }
+  }
+}
+'''
+
+
+def github_query_runner(github_username, query):
     query_reponse = requests.post(
         url='https://api.github.com/graphql',
         auth=(
@@ -42,8 +65,18 @@ def main(github_username):
     data = query_reponse.json()
     if 'errors' in data:
         print(data)
-    data=data['data']['user']
-    output = {key:data[key]['totalCount'] for (key,value) in data.items()}
+    return data
+
+
+def main(github_username):
+    unpaginated_data = github_query_runner(github_username, unpaginated_query)
+    unpaginated_data=unpaginated_data['data']['user']
+    output = {key:unpaginated_data[key]['totalCount'] for (key,value) in unpaginated_data.items()}
+
+    paginated_data = github_query_runner(github_username, paginated_query)
+    paginated_data=paginated_data['data']['user']
+    import pprint;pprint.pprint(paginated_data)
+
     return output
 
 
