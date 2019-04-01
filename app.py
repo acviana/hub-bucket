f502f96a5ec8522b2324d6f4c3627a33cd39f6c6
Alex C. Viana <alexcostaviana@gmail.com>
2019-04-01 11:49:04 -0500

    First working graphQL query.

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
+import os
+
 import requests
 
 
 def main():
-    pass
+    query='''
+    {
+      user(login: "kenneth-reitz") {
+      totalRepositories: repositories {
+        totalCount
+        }
+      }
+    }
+    '''
+    query_reponse = requests.post(
+        url='https://api.github.com/graphql',
+        auth=(
+            os.environ['HUBBUCKET_AUTH_USERNAME'],
+            os.environ['HUBBUCKET_AUTH_TOKEN']
+        ),
+        json={'query': query}
+    )
+    print(query_reponse.json())
 
 
 if __name__ == '__main__':
