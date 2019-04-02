76cc52943c0cbcf74b907d85f2c94c39845c5386
Alex C. Viana <alexcostaviana@gmail.com>
2019-04-01 19:25:33 -0500

    Added query validation to flask API

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import (
+    Flask,
+    jsonify,
+    request,
+)
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/api/v1/user")
 def hello():
-    return "Hello World!"
+    try:
+        github_username = request.args['github_username']
+        bitbucket_username = request.args['bitbucket_username']
+    except KeyError:
+        return jsonify(
+            {
+                'Error': (
+                    'github_username and bitbucket_username are both '
+                    'required query paramteres. e.g.: '
+                    'api/v1/user?github_username=user1&bitbucket_username=user2'
+                )
+            }
+        )
+    else:
+        return jsonify(
+                {
+                    'github_username': github_username,
+                    'bitbucket_username': bitbucket_username,
+                }
+            )
+
