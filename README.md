9b1a41c7a276137624f2b71fadbcc233c77d5f12
Alex C. Viana <alexcostaviana@gmail.com>
2019-04-01 20:19:39 -0500

    Working version of Flask app for GitHub data.

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ Make sure you have pipenv and Python 3.6 installed.
 $ pipenv install
 ```
 
-
 **GitHub Keys**
 
 GitHub enforces a API limit of 60 requests per hour per IP address. Authenticated users can make 5,000 requests per hour. If you create [personal access tokens](https://github.blog/2013-05-16-personal-api-tokens/) you can set these as environment variables to make authenticated requests.
@@ -30,4 +29,20 @@ $ export HUBBUCKET_AUTH_USERNAME='my-username'
 
 $ echo $HUBBUCKET_AUTH_USERNAME
 my-username
+```
+
+**Running the Service**
+
+Once you have your keys configured you can launch the Flask app in the same shell session by running:
+
+```shell
+$ flask run
+```
+
+The following url `http://127.0.0.1:5000/api/v1/user?github_username=kenneth-reitz&bitbucket_username=2`
+
+Which should return:
+
+```
+http://127.0.0.1:5000/api/v1/user?github_username=kenneth-reitz&bitbucket_username=2
 ```
--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import (
     jsonify,
     request,
 )
+from werkzeug.http import HTTP_STATUS_CODES
+
+from github_v4 import main
 
 app = Flask(__name__)
+
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
 
 
 @app.route("/api/v1/user")
@@ -13,20 +25,18 @@ def hello():
         github_username = request.args['github_username']
         bitbucket_username = request.args['bitbucket_username']
     except KeyError:
-        return jsonify(
+        message = (
+            'github_username and bitbucket_username are both required '
+            'query paramteres. e.g.: '
+            'api/v1/user?github_username=user1&bitbucket_username=user2'
+        )
+        return error_response(400, message)
+
+    return jsonify(
             {
-                'Error': (
-                    'github_username and bitbucket_username are both '
-                    'required query paramteres. e.g.: '
-                    'api/v1/user?github_username=user1&bitbucket_username=user2'
-                )
+                'github_data': main(github_username),
+                'github_username': github_username,
+                'bitbucket_username': bitbucket_username,
             }
         )
-    else:
-        return jsonify(
-                {
-                    'github_username': github_username,
-                    'bitbucket_username': bitbucket_username,
-                }
-            )
 
